058b17158e1cf9b3fa75dcb2705f94027eefbab2
bigredbug <31086329+bigredbug47@users.noreply.github.com>
2017-08-17 10:40:01 +0700

    Add files via upload

---
+++ Source/Regex_Tester_Project/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Regex_Tester_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.exportFile = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.Error_Box = new System.Windows.Forms.TextBox();
+            this.result_tester = new System.Windows.Forms.RichTextBox();
+            this.input_pattern = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.exportFile);
+            this.panel1.Controls.Add(this.openButton);
+            this.panel1.Controls.Add(this.Error_Box);
+            this.panel1.Controls.Add(this.result_tester);
+            this.panel1.Controls.Add(this.input_pattern);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1050, 410);
+            this.panel1.TabIndex = 0;
+            // 
+            // exportFile
+            // 
+            this.exportFile.BackColor = System.Drawing.Color.Snow;
+            this.exportFile.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.exportFile.Location = new System.Drawing.Point(968, 28);
+            this.exportFile.Name = "exportFile";
+            this.exportFile.Size = new System.Drawing.Size(75, 23);
+            this.exportFile.TabIndex = 3;
+            this.exportFile.Text = "Save";
+            this.exportFile.UseVisualStyleBackColor = false;
+            this.exportFile.Click += new System.EventHandler(this.exportFile_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.BackColor = System.Drawing.Color.Snow;
+            this.openButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.openButton.Location = new System.Drawing.Point(887, 28);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 3;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = false;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // Error_Box
+            // 
+            this.Error_Box.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Error_Box.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Error_Box.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Error_Box.Location = new System.Drawing.Point(951, 64);
+            this.Error_Box.Name = "Error_Box";
+            this.Error_Box.Size = new System.Drawing.Size(88, 19);
+            this.Error_Box.TabIndex = 2;
+            this.Error_Box.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // result_tester
+            // 
+            this.result_tester.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.result_tester.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result_tester.Location = new System.Drawing.Point(10, 91);
+            this.result_tester.Name = "result_tester";
+            this.result_tester.Size = new System.Drawing.Size(1033, 306);
+            this.result_tester.TabIndex = 1;
+            this.result_tester.Text = "";
+            this.result_tester.TextChanged += new System.EventHandler(this.result_tester_TextChanged);
+            // 
+            // input_pattern
+            // 
+            this.input_pattern.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.input_pattern.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.input_pattern.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input_pattern.Location = new System.Drawing.Point(10, 62);
+            this.input_pattern.MaxLength = 30000;
+            this.input_pattern.Name = "input_pattern";
+            this.input_pattern.Size = new System.Drawing.Size(1034, 23);
+            this.input_pattern.TabIndex = 0;
+            this.input_pattern.TextChanged += new System.EventHandler(this.input_pattern_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Mistral", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(10, 10);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(403, 46);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "REGEX TESTER VIETNAMESE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 411);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Regex Tester Vietnamese";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox input_pattern;
+        private System.Windows.Forms.RichTextBox result_tester;
+        private System.Windows.Forms.TextBox Error_Box;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button exportFile;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Source/Regex_Tester_Project/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+namespace Regex_Tester_Project
+{
+    public partial class Form1 : Form
+    {
+        Regex regex_standard = new Regex("^(?:(?:[^?+*{}()[\\]\\\\|]+|\\\\.|\\[(?:\\^?\\\\.|\\^[^\\\\]|[^\\\\^])(?:[^\\]\\\\]+|\\\\.)*\\]|\\((?:\\?[:=!]|\\?<[=!]|\\?>|\\?<[^\\W\\d]\\w*>|\\?'[^\\W\\d]\\w*')?(?<N>)|\\)(?<-N>))(?:(?:[?+*]|\\{\\d+(?:,\\d*)?\\})[?+]?)?|\\|)*$(?(N)(?!))");
+        string unicode_test = "\\u";
+        string unicode_key = "(á|à|ạ|ả|ã|ă|ắ|ằ|ặ|ẳ|ẵ|â|ấ|ầ|ậ|ẩ|ẫ|đ|é|è|ẹ|ẻ|ẽ|ê|ế|ề|ệ|ể|ễ|í|ì|ị|ỉ|ĩ|ó|ò|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ớ|ờ|ợ|ở|ỡ|ú|ù|ụ|ủ|ũ|ư|ứ|ừ|ự|ử|ữ|ý|ỳ|ỵ|ỷ|ỹ|Á|À|Ạ|Ả|Ã|Ă|Ắ|Ằ|Ặ|Ẳ|Ẵ|Â|Ấ|Ầ|Ậ|Ẩ|Ẫ|Đ|É|È|Ẹ|Ẻ|Ẽ|Ê|Ế|Ề|Ệ|Ể|Ễ|Í|Ì|Ị|Ỉ|Ĩ|Ó|Ò|Ọ|Ỏ|Õ|Ô|Ố|Ồ|Ộ|Ổ|Ỗ|Ơ|Ớ|Ờ|Ợ|Ở|Ỡ|Ú|Ù|Ụ|Ủ|Ũ|Ư|Ứ|Ừ|Ự|Ử|Ữ|Ý|Ỳ|Ỵ|Ỷ|Ỹ)";
+        int temp = 0;
+        public Form1()
+        {
+            InitializeComponent();              
+        }
+
+        string Vietnamese(string S)
+        {
+            List<int> lPosition = new List<int>();
+            int position = 0;
+
+            for (position = 2; position < S.Length - 2; position++)
+            {
+                if (S[position] == '-' && S[position - 1] != '0' && S[position - 1] != '1' && S[position - 1] != '\\' && S[position - 1] != '2' && S[position - 1] != '3' && S[position - 1] != '4' && S[position - 1] != '5' && S[position - 1] != '6' && S[position - 1] != '7' && S[position - 1] != '8' && S[position - 1] != '9' && S[position + 1] != '0' && S[position + 1] != '1' && S[position + 1] != '2' && S[position + 1] != '3' && S[position + 1] != '4' && S[position + 1] != '5' && S[position + 1] != '6' && S[position + 1] != '7' && S[position + 1] != '8' && S[position + 1] != '9' && S[position + 1] != '\\')
+                    if (S[position] == '-')
+                        lPosition.Add(position);
+            }
+            string az = "aáàạảãăắằặẳẵâấầậẩẫbcdđeéèẹẻẽêếềệểễfghiíìịỉĩjklmnoóòọỏõôồốộổỗơớờợởỡpqrstuúùụủũưứừựửữvwxyýỳỵỷỹzAÁÀẠẢÃĂẮẰẶẲẴÂẤẦẬẨẪBCDĐEÉÈẸẺẼÊẾỀỆỂỄFGHIÍÌỊỈĨJKLMNOÓÒỌỎÕÔỐỒỘỔỖƠỚỜỢỞỠPQRSTUÚÙỤỦŨƯỨỪỰỬỮVWXYÝỲỴỶỸZ";
+
+            char cStart = 'a';
+            char cEnd = 'z';
+            string sNew = "";
+            int n = lPosition.Count();
+            for (int i = n - 1; i >= 0; i--)
+            {
+                sNew = "";
+                cStart = S[lPosition[i] - 1];
+                cEnd = S[lPosition[i] + 1];
+                int a = az.IndexOf(cStart);
+                int b = az.IndexOf(cEnd);
+                for (int j = a; j < b; j++)
+                {
+                    sNew = sNew + az[j] + '|';
+                }
+                sNew = sNew + cEnd;
+                S = S.Replace(cStart + "-" + cEnd, sNew);
+
+            }
+            return S;
+        }
+        private void result_tester_TextChanged(object sender, EventArgs e)
+         {
+            regex_analyst();
+        }
+
+        public void regex_analyst()
+        {
+            string pattern_check = input_pattern.Text;
+            string result_check = result_tester.Text;
+            Match check_regex_standard = regex_standard.Match(pattern_check);
+            if (check_regex_standard.Groups[0].Value != "")
+            {
+                pattern_check = Vietnamese(pattern_check);
+                pattern_check = pattern_check.Replace(unicode_test, unicode_key);
+                pattern_check = pattern_check.Replace("\\U", "[a-z]|[A-Z]");
+                Regex rx = new Regex(pattern_check, RegexOptions.Compiled);
+                Match match = rx.Match(result_check);
+                if (match.Groups[0].Value == "")
+                {
+                    Error_Box.Text = "NOT EXIST";
+                    Error_Box.BackColor = Color.Red;
+                }
+                else
+                {
+                    Error_Box.Text = "";
+                    Error_Box.BackColor = SystemColors.ActiveBorder;
+                    while (match.Groups[0].Value != "")
+                    {
+                        result_tester.Select(match.Index, match.Length);
+                        result_tester.SelectionBackColor = Color.Yellow;
+                        match = match.NextMatch();
+                        temp = 1;
+                    }
+                    result_tester.Select(result_check.Length, result_check.Length);
+                    result_tester.SelectionBackColor = SystemColors.ActiveBorder;
+                    temp = 0;
+                }
+            }
+        }
+
+        private void input_pattern_TextChanged(object sender, EventArgs e)
+        {
+            regex_analyst();
+            if (temp == 0)
+            {
+                result_tester.SelectAll();
+                result_tester.SelectionBackColor = SystemColors.ActiveBorder;
+            }
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "TXT|*.txt";
+            ofd.Title = "Mở file";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader read = new StreamReader(File.OpenRead(ofd.FileName));
+                result_tester.Text = read.ReadToEnd();
+                read.Dispose();
+            }
+        }
+
+        private void exportFile_Click(object sender, EventArgs e)
+        {
+            string newFindBox = Vietnamese(input_pattern.Text);
+            Regex regg = new Regex(newFindBox);
+            Match match = Regex.Match(result_tester.Text, newFindBox);
+            string a = "";
+            while (match.Groups[0].Value != "")
+            {
+                a = a + match.Value + "\n";
+                match = match.NextMatch();
+            }
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = ".TXT|*.txt";
+            sfd.Title = "Xuất file";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter write = new StreamWriter(File.Create(sfd.FileName));
+                write.Write(a);
+                write.Dispose();
+            }
+        }
+
+    }
+}
